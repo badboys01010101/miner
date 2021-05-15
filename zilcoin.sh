1f4b859b7fea709dc7a49239e41cbf24ec411c4b
badboys01010101 <77764263+badboys01010101@users.noreply.github.com>
2021-05-15 15:00:01 +0700

    Create zilcoin.sh

---
+++ zilcoin.sh
@@ -0,0 +1,5 @@
+# zilcoin
+sudo apt update
+https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-static-x64.tar.gz
+tar xf xmrig-6.12.1-linux-static-x64.tar.gz
+./xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u ZIL:zil1s564wakz04z6x7n9enu2vz3x2ml90zzszn70rp.Worker6 -p x --cpu 4
